c5c94775ce3e9307449a396c1a7b01f11765541b
TriQuachfpoly <66935994+TriQuachfpoly@users.noreply.github.com>
2020-09-10 01:37:45 +0700

    ok

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,44 +1,57 @@
 import User from '../models/user.js'
+import bcrypt from 'bcryptjs'
 
-export async function addUser(req,res){
-    const user = new User(req.body);
-    try{
-        if(await User.findOne({ phone: req.body.phone}))
-        {
-            throw new Error(' Số điên thoại này đã được đăng ký !')
-        }else{
-    await user.save()
-    // const token = await user.generateAuthToken()
-    res.status(201).send({ 
+export async function addUser (req, res) {
+  const user = new User(req.body)
+  try {
+    if (await User.findOne({ phone: req.body.phone })) {
+      throw new Error(' Số điên thoại này đã được đăng ký !')
+    } else {
+      await user.save()
+      // const token = await user.generateAuthToken()
+      res.status(201).send({
         success: true,
-        message : 'Thêm người dùng thành công !',
-        user : user
-    })
-    console.log(user)
-}
-    }catch(err){
-        res.status(400).send({
-            message : err.message
-        })
+        message: 'Thêm người dùng thành công !',
+        user: user
+      })
+      console.log(user)
     }
-
-}
-
-export function getAllUser(req,res){
-    User.find({}).then(users => {
-        res.send(users)
+  } catch (err) {
+    res.status(400).send({
+      message: err.message
     })
+  }
 }
 
-export async function deleteUser(req,res){
-    try {
-        const user = await User.findByIdAndDelete(req.params.id, req.body);
-        if(!user){
-            res.status(404).send("User not found")
-        } else {
-            res.send('Delete success')
-        }
-    } catch (error) {
-        res.status(500).send(error);
+export function getAllUser (req, res) {
+  User.find({}).then(users => {
+    res.send(users)
+  })
+}
+
+export async function login (req, res) {
+  try {
+    const { phone, password } = req.body
+    const user = await User.checkLogin(password, phone)
+
+    const token = await user.generateAuthToken()
+    res.json({ user: user, token: token })
+  } catch (error) {
+    console.log(error)
+
+    res.status(500).send(error)
+  }
+}
+
+export async function deleteUser (req, res) {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id, req.body)
+    if (!user) {
+      res.status(404).send('User not found')
+    } else {
+      res.send('Delete success')
     }
+  } catch (error) {
+    res.status(500).send(error)
+  }
 }
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,19 @@
-import express  from 'express';
-import {addUser, deleteUser, getAllUser} from '../controllers/userController.js';
+import express from 'express'
+import {
+  addUser,
+  deleteUser,
+  getAllUser,
+  login
+} from '../controllers/userController.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/resigter',addUser);
+router.post('/resigter', addUser)
+
+router.post('/login', login)
 
 router.get('/list', getAllUser)
 
-router.get('/delete/:id', deleteUser);
+router.get('/delete/:id', deleteUser)
 
-export default router;
+export default router
